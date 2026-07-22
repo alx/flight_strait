3c38f3bbe1b25d65a1d45d7ca256d070a327fcf3
SamuiBuilder <girard.davila@gmail.com>
2026-07-22 16:39:40 +0200

    Add Hugo templates for homepage and day pages

---
+++ static/js/map.js
@@ -0,0 +1,23 @@
+(function () {
+  var data = JSON.parse(document.getElementById("flight-data").textContent);
+  var refs = window.CASPIAN_REFERENCE_POINTS;
+
+  var map = L.map("map").setView([refs.baku[0], (refs.baku[1] + refs.turkmenbashi[1]) / 2], 6);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors",
+  }).addTo(map);
+
+  L.marker(refs.baku).addTo(map).bindPopup("Baku");
+  L.marker(refs.turkmenbashi).addTo(map).bindPopup("Turkmenbashi");
+
+  var colors = ["#e6194b", "#3cb44b", "#4363d8", "#f58231", "#911eb4", "#46f0f0"];
+
+  (data.flights || []).forEach(function (flight, i) {
+    var points = flight.track.map(function (p) {
+      return [p.lat, p.lon];
+    });
+    L.polyline(points, { color: colors[i % colors.length] })
+      .addTo(map)
+      .bindPopup(flight.flight || flight.hex);
+  });
+})();
